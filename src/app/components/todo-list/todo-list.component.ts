fba9fedb7a14c07a05a060d3aebbcdee4a149eca
Damian Osiecki <d.osiecki@direktpoint.pl>
2020-01-06 15:02:10 +0100

    Add event add feature

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -7,15 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
   todos: object[];
+  todoName: string;
+  idForTodo: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.idForTodo = 4;
+    this.todoName = '';
     this.todos = [
       { 'id': 0, 'name': "Event 1", 'completed': false, 'editing': false, },
       { 'id': 1, 'name': "Event 2", 'completed': false, 'editing': false, },
       { 'id': 2, 'name': "Event 3", 'completed': false, 'editing': false, },
     ];
   }
+  addTodo(): void {
+    if (this.todoName.trim().length === 0) {
+      return;
+    }
 
+    this.todos.push({
+      id: this.idForTodo,
+      name: this.todoName,
+      completed: false,
+      editing: false
+    })
+
+    this.todoName = '';
+    this.idForTodo++;
+  }
 }
