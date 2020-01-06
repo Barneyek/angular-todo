d0301b7315ed345a19cf6c5a0b56eb57fdeeafbc
Damian Osiecki <d.osiecki@direktpoint.pl>
2020-01-06 13:16:35 +0100

    Render data on html page

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  todos: object[];
 
   constructor() { }
 
   ngOnInit() {
+    this.todos = [
+      { 'id': 0, 'name': "Event 1", 'completed': false, 'editing': false, },
+      { 'id': 1, 'name': "Event 2", 'completed': false, 'editing': false, },
+      { 'id': 2, 'name': "Event 3", 'completed': false, 'editing': false, },
+    ];
   }
 
 }
